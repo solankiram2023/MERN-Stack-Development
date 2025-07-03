d2ef4d4c65e0d8a5021b00a66589daa7b5e3cbb6
solankiram2023 <145072212+solankiram2023@users.noreply.github.com>
2025-07-03 04:37:02 -0400

    Used State in the NewGoal component, to make changes and reflect it in the UI by applying State and making functions to store it in UI

--- my-app/src/components/NewGoal/NewGoal.js
+++ my-app/src/components/NewGoal/NewGoal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewGoal.css';
 
 const NewGoal = props => {
+    //let enteredText = '';
+    //made a variable to store the input value
+    const [enteredText, setEnteredText] = useState('');
+
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'My new goal!'
+            text: enteredText
 
         };
 
+        setEnteredText('');
+        //enteredText = ''; //reset the input value after submission
+        
+
         props.onAddGoal(newGoal);
     };
+
+    const textChangeHandler = event => {
+        setEnteredText(event.target.value);
+    };
+
     return(<form className="new-goal" onSubmit={addGoalHandler}>
-        <input type="text" /> 
+        <input type="text" value={enteredText} onChange={textChangeHandler} /> 
         <button type="submit">Add Goal</button>
     </form>
     );
