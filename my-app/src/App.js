801dbb3625a5f04e065e6629d1abc23f96a141c1
solankiram2023 <145072212+solankiram2023@users.noreply.github.com>
2025-07-03 01:45:39 -0400

    UseState concept used which states that whenever an event occurse react does not rerender the jsx code in real UI, so we need to explicitly tell react to rerender the entire screen to make sure not data is missed, which is also called React Hook functions which are used to manage states in React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import GoalList from './components/GoalList/GoalList.js';
 import NewGoal from './components/NewGoal/NewGoal.js';
@@ -6,17 +6,18 @@ import NewGoal from './components/NewGoal/NewGoal.js';
 import './App.css';
 
 const App = () => {
-  const courseGoals = [
+
+  const [courseGoals, setCourseGoals] = useState([
     {id: 'cg1', text: 'Finish the Course'},
     {id: 'cg2', text: 'Learn all about the course main topic'},
-    {id: 'cg3', text: 'Help other students in the course Q&A'},
+    {id: 'cg3', text: 'Help other students in the course Q&A'}
 
-  ];
+  ]);
+ 
 
-  const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
-  }
+  const addNewGoalHandler = newGoal => {
+    setCourseGoals(courseGoals.concat(newGoal));
+  };
 
 
 
